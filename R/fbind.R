1940bf086d052d3737f7c2709ffb99c3c61069cb
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-07-18 19:33:55 -0700

    Added a roxygen comment for fbind()

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,17 @@
+#' Bind two factors
+#'
+#' I could say more. This is called the 'description'.
+#'
+#' @param a A factor.
+#' @param b A factor.
+#'
+#' @return A factor.
+#' @export
+#'
+#' @examples
+#' a <- factor(c("character", "hits", "your", "eyeballs"))
+#' b <- factor(c("but", "integer", "where it", "counts"))
+#' fbind(a, b)
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
